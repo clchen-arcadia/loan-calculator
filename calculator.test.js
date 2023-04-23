03709bacc477267eaeae0f5228ee339b85445f66
clchen-arcadia <clchen.arcadia@gmail.com>
2023-04-22 20:30:13 -0400

    Fix formatting

--- calculator.test.js
+++ calculator.test.js
@@ -1,4 +1,4 @@
 it('should calculate monthly rate', function () {
-  expect(calcMonthlyPayment(10000,10,0.045)).toEqual(103.63840875701705);
+  expect(calcMonthlyPayment(10000, 10, 4.5)).toEqual(103.63840875701705);
   //correcting setting 4.5% to 0.045 handled in getFormValues()
 });
